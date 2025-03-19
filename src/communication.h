d1b6d704216c8da860fa664be71f2fb71fbf7de6
Lukas Stürmlinger <lukas.stuermlinger@sew-eurodrive.de>
2025-03-19 09:45:01 +0100

    can Id configurable in yaml

--- src/communication.h
+++ src/communication.h
@@ -26,9 +26,9 @@ static const CanMember ESPClient{ESPCLIENT_ID, "ESPClient"};
 static const CanMember Manager{MANAGER_ID, "Manager"};
 static const CanMember Kessel{KESSEL_ID, "Kessel"};
 static const CanMember HK1{HK1_ID, "HK1"};
-static const CanMember FET{0x601, "FET"};    // TODO: make configurable and double check
-static const CanMember MFG{0x700, "MFG"};    // TODO: make configurable and double check
-static const CanMember WPM2{0x480, "WPM2"};  // TODO: make configurable and double check
+static const CanMember FET{FET_ID, "FET"};
+static const CanMember MFG{MFG_ID, "MFG"};
+static const CanMember WPM2{WPM_ID, "WPM2"};
 
 static const std::vector<std::reference_wrapper<const CanMember>> canMembers{Kessel, HK1, Manager, ESPClient,
                                                                              FET,    MFG, WPM2};
